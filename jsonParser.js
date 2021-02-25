133cb413daadf8ef95f3465199a406d8a26f649d
Alex <alexkara@hcl.com>
2021-02-25 14:40:19 +0100

    added README.md and updated package.json

--- jsonParser.js
+++ jsonParser.js
@@ -1,16 +1,14 @@
 const fs =require('fs');
-const Application=require("./objects/application.js")
 const appExportPath="./static/testApplication.json"
 
 fs.readFile (appExportPath,(err,data)=>{
     if (err) throw err;
     const tempData =JSON.parse(data);
-    application=new Application(tempData.name);
-    console.log("Application Name : " + application.name);
+    console.log("Application Name : " + tempData.name);
 
     const plugins =new Map();
 
-/*Get the list of components */
+/*Get and print the list of plugins used by this application */
 const components=[];
     for (const comp of tempData.components) {
         components.push(comp);
@@ -19,21 +17,19 @@ const components=[];
              if(proc.rootActivity!=null){
             for (let index = 1; index < proc.rootActivity.children.length; index++) {
                 pluginName=proc.rootActivity.children[index].pluginName;
-                pluginVersion=proc.rootActivity.children[index].pluginVersion;
-                
-                if (plugins.has(pluginName)&plugins.get(pluginName)!=pluginVersion){
-                    console.log("There are multiple versions of plugin : " + pluginName + ": " + pluginVersion + " => Path: " + proc.path );
+                pluginVersion=proc.rootActivity.children[index].pluginVersion;                
+                if (plugins.has(pluginName)&plugins.get(pluginName)!=pluginVersion ){
+                    console.log("There are multiple versions of plugin : " + pluginName + ": " + pluginVersion + " ==> Path: " + proc.path );
+                    
                     break;
                 }
-            plugins.set(pluginName,pluginVersion)    
+            plugins.set(pluginName , pluginVersion)    
         }}
          
-    }
-        
+    }     
     } 
-
     plugins.forEach(function(value, key) {
-        console.log(key + ' v' + value)
+        console.log(key + ' v ' + value)
       })
 
  /*   components.forEach(function(comp) {
